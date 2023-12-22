cb191766d4ddbe8eebc285f0726428422cf9fc05
binyam <btechan@gmail.com>
2023-12-22 15:47:31 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Using Puppet, install flask from pip3.
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Using Puppet, install flask & werkzeug from pip3.
+exec { 'install-werkzeug':
+  command => 'pip3 install werkzeug==2.1.1',
+  path    => ['/usr/bin', 'usr/local/bin']
+}
+exec {'install-flask':
+  command => 'pip3 install Flask==2.1.0',
+  require => Exec['install-werkzeug'],
+  path    => ['/usr/bin', 'usr/local/bin']
 }
