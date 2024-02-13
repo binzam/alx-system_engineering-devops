5be77bdc1026df307eaef83014d360b318d648fe
root <btechan@gmail.com>
2024-02-13 10:00:35 -0800

    ...

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subs, 0 if the given subreddit is invalid."""
-    headers = {'User-Agent': 'A-fethi/1.0'}
+    headers = {'User-Agent': 'BinzamAlx'}
     r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
                      headers=headers, allow_redirects=False)
     if r.status_code == 200:
